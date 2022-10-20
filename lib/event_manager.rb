7624202dc35fd529bd1aed54ab3f26f96f2642ce
missopi <missopi@me.com>
2022-10-20 15:45:54 +0100

    Fix issue with inconsistent zipcodes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -11,9 +11,13 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
 
-# if the zipcode is 5 digits its ok
-# if the zipcode has more than 5 digits, reduce it to the first 5 digits
-# if the zipcode is less than 5 digits add 00s to the start until it is 5 digits
+  if zipcode.nil?
+    zipcode = '00000'
+  elsif zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  end
 
   puts "#{name} #{zipcode}"
 end
