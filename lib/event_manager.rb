442e56211330d726d78028bdafc73d0143e41e7f
missopi <missopi@me.com>
2022-10-20 15:33:00 +0100

    Covert header names all to symbols for uniformity

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,12 @@
 require 'csv'
 puts 'Event Manager Initialized!'
 
-contents = CSV.open('event_attendees.csv', headers: true)
+contents = CSV.open(
+  'event_attendees.csv', 
+  headers: true,
+  header_converters: :symbol
+)
+
 contents.each do |row|
   name = row[2]
   puts name
