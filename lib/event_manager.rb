f5d4591537c190c50d728bc65b692e4ee972a89a
missopi <missopi@me.com>
2022-10-20 15:40:36 +0100

    Peudocode to sort out how to solve issue

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -10,6 +10,11 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+# if the zipcode is 5 digits its ok
+# if the zipcode has more than 5 digits, reduce it to the first 5 digits
+# if the zipcode is less than 5 digits add 00s to the start until it is 5 digits
+
   puts "#{name} #{zipcode}"
 end
 
