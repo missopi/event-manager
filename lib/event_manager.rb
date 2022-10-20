ba73744cf229a175f1eba94b77559a93ce1b62ce
missopi <missopi@me.com>
2022-10-20 15:22:40 +0100

    Split individual lines and columns of data
    Isolate first_names column

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,11 @@
 puts 'Event Manager Initialized!'
 
-contents = File.read('event_attendees.csv')
-puts contents
+lines = File.readlines('event_attendees.csv')
+lines.each do |line|
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
+
+
+
