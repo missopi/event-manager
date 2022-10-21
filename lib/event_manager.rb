a375c0d7497cb82d3fe810a0743abf65da9c1df0
missopi <missopi@me.com>
2022-10-21 20:52:22 +0100

    Fix typo in legislator method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-puts 'Event Manager Initialized!'
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
@@ -10,19 +8,22 @@ end
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-  begin 
+  
+  begin
     legislators = civic_info.representative_info_by_address(
-      address: zipcode,
+      address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
     )
     legislators = legislators.officials
     legislator_names = legislators.map(&:name)
-    legislators_string = legislator_names.join(", ")
+    legislator_names.join(", ")
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
+
+puts 'Event Manager Initialized!'
 
 contents = CSV.open(
   'event_attendees.csv', 
