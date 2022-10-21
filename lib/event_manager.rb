5e1d94c202da5c7b9197129d1af9fdd1e0849a88
missopi <missopi@me.com>
2022-10-21 14:42:43 +0100

    Access legislator info

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,9 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
 puts 'Event Manager Initialized!'
 
 def clean_zipcode(zipcode)
@@ -16,6 +19,12 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  legislators = civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country',
+    roles: ['legislatorUpperBody', 'legislatorLowerBody']
+  )
+  legislators = legislators.officials
   puts "#{name} #{zipcode}"
 end
 
